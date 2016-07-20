0e855cca72229bd7cae0ba0fc0bba879930a6e50
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-07-21 07:03:11 +0900

    es6 to es5

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(function (req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
@@ -16,6 +16,6 @@ const server = http.createServer((req, res) => {
 */
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, hostname, function () {
+  console.log('Server running at http://' + hostname + ':' + port + '/');
 });
