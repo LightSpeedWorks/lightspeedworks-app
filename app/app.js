237cfba720e841257ce60e78169d6252b1f61e8d
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-07-21 06:58:53 +0900

    revise

--- app/app.js
+++ app/app.js
@@ -6,11 +6,14 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+/*
   res.end(['Hello World',
     '__dirname = ' + __dirname,
     '__filename = ' + __filename,
     'process.cwd() = ' + process.cwd(),
   ].join('\n'));
+*/
 });
 
 server.listen(port, hostname, () => {
