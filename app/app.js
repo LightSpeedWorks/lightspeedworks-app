59d251fd15852e3e93215bf832371394b2a2dee2
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-07-21 07:12:11 +0900

    revise

--- app/app.js
+++ app/app.js
@@ -1,21 +1,32 @@
-const http = require('http');
+'use strict';
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3000;
+try {
+	console.log('app.js starting...');
 
-const server = http.createServer(function (req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-/*
-  res.end(['Hello World',
-    '__dirname = ' + __dirname,
-    '__filename = ' + __filename,
-    'process.cwd() = ' + process.cwd(),
-  ].join('\n'));
-*/
-});
+	const http = require('http');
 
-server.listen(port, hostname, function () {
-  console.log('Server running at http://' + hostname + ':' + port + '/');
-});
+	const hostname = '127.0.0.1';
+	const port = process.env.PORT || 3000;
+
+	const server = http.createServer(function (req, res) {
+		console.log(req.method, req.url);
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('Hello World\n');
+	/*
+		res.end(['Hello World',
+			'__dirname = ' + __dirname,
+			'__filename = ' + __filename,
+			'process.cwd() = ' + process.cwd(),
+		].join('\n'));
+	*/
+	});
+
+	server.listen(port, hostname, function () {
+		console.log('Server running at http://' + hostname + ':' + port + '/');
+	});
+}
+catch (e) {
+	console.warn(e);
+	console.error(e.stack);
+}
